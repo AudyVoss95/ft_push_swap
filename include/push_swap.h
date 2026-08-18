23283a48ecb840c73787284d6b6ce81a44847f65
andremarques9 <105232663+andremarques9@users.noreply.github.com>
2026-08-18 15:35:48 -0300

    working on the 11 operations

--- include/push_swap.h
+++ include/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: audgiova <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: andmarqu <andmarqu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/30 13:10:20 by audgiova          #+#    #+#             */
-/*   Updated: 2026/07/30 14:00:55 by audgiova         ###   ########.fr       */
+/*   Updated: 2026/08/10 12:59:30 by andmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,32 @@
 # define PUSH_SWAP_H
 
 # include "libft.h"
+
+typedef struct	s_list
+{
+	int				value;
+	int				index;
+	struct s_list	*prev;
+	struct s_list	*next;
+}	t_list;
+
+typedef struct	s_stack
+{
+	t_list	*top;
+	t_list	*bottom;
+	int		size;
+}	t_stack;
+
+void sa(t_stack *stack);
+void sb(t_stack *stack);
+void ss(t_stack *a, t_stack *b);
+void pa(t_stack *a, t_stack *b);
+void pb(t_stack *a, t_stack *b);
+void ra(t_stack *stack);
+void rb(t_stack *stack);
+void rr(t_stack *a, t_stack *b);
+void rra(t_stack *stack);
+void rrb(t_stack *stack);
+void rrr(t_stack *a, t_stack *b);
 
 #endif
---
+++ src/reverse_rotate.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_rotate.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andmarqu <andmarqu@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/10 18:46:34 by andmarqu          #+#    #+#             */
+/*   Updated: 2026/08/10 18:46:34 by andmarqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	reverse_rotate_stack(t_stack *stack)
+{
+	t_list	*first;
+	t_list	*last;
+	t_list	*new_last;
+
+	if (stack == NULL || stack->size < 2)
+	{
+		return ;
+	}
+	first = stack->top;
+	last = stack->bottom;
+	new_last = last->prev;
+	last->next = first;
+	last->prev = NULL;
+	first->prev = last;
+	new_last->next = NULL;
+	stack->top = last;
+	stack->bottom = new_last;
+}
+
+void rra(t_stack *stack)
+{
+	reverse_rotate_stack(stack);
+	ft_printf("rra\n");
+}
+
+void rrb(t_stack *stack)
+{
+	reverse_rotate_stack(stack);
+	ft_printf("rrb\n");
+}
+
+void rrr(t_stack *a, t_stack *b)
+{
+	reverse_rotate_stack(a);
+	reverse_rotate_stack(b);
+	ft_printf("rrr\n");
+}
