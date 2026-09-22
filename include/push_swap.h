d5786e0efdb26475b2dd50ee5dd2d0d03bf5a422
Audy <audgiova@student.42sp.org.br>
2026-09-22 18:43:46 -0300

    submit 00

--- include/push_swap.h
+++ include/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: andmarqu <andmarqu@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: audgiova <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/30 13:10:20 by audgiova          #+#    #+#             */
-/*   Updated: 2026/08/10 12:59:30 by andmarqu         ###   ########.fr       */
+/*   Updated: 2026/07/30 14:00:55 by audgiova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,5 @@
 # define PUSH_SWAP_H
 
 # include "libft.h"
-
-typedef struct	s_list
-{
-	int				value;
-	int				index;
-	struct s_list	*prev;
-	struct s_list	*next;
-}	t_list;
-
-typedef struct	s_stack
-{
-	t_list	*top;
-	t_list	*bottom;
-	int		size;
-}	t_stack;
-
-void sa(t_stack *stack);
-void sb(t_stack *stack);
-void ss(t_stack *a, t_stack *b);
-void pa(t_stack *a, t_stack *b);
-void pb(t_stack *a, t_stack *b);
-void ra(t_stack *stack);
-void rb(t_stack *stack);
-void rr(t_stack *a, t_stack *b);
-void rra(t_stack *stack);
-void rrb(t_stack *stack);
-void rrr(t_stack *a, t_stack *b);
 
 #endif
